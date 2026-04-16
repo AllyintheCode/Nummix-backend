8e2e6bffe76f406befdc2df6cab3c3a1ce2c31f0
AllyintheCode <allyasadzade@gmail.com>
2026-04-16 16:49:09 +0400

    index.js changed

--- index.js
+++ index.js
@@ -46,9 +46,8 @@ const app = express();
 app.set("trust proxy", 1); // bu X-Forwarded-For header üçün mütləqdir
 
 // --- Middlewares ---
-app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend-in portu
+  origin: '*',
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
@@ -125,14 +124,13 @@ app.use("/api/reports", reportRoutes);
 
 
 // --- DB & Server Start ---
-const startServer = async () => {
-  try {
-    await connectDB();
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server ${PORT}-da işləyir 🚀`));
-  } catch (error) {
-    console.error("Server başlaya bilmədi:", error.message);
-  }
-};
+const PORT = process.env.PORT || 5000;
 
-startServer();
+// Portu əvvəlcə aç, sonra DB-yə qoşul
+app.listen(PORT, () => {
+  console.log(`Server ${PORT}-da işləyir 🚀`);
+  // MongoDB-yə ayrı qoşulma
+  connectDB().catch((err) => {
+    console.error("MongoDB qoşulma xətası:", err.message);
+  });
+});
